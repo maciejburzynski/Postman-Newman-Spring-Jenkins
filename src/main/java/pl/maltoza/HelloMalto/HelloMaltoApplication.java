10b1018e64eafd950f4f682b327a6fd25bfd3ce1
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-04-04 21:42:34 +0200

    Add Spring init

---
+++ src/main/java/pl/maltoza/HelloMalto/HelloMaltoApplication.java
@@ -0,0 +1,13 @@
+package pl.maltoza.HelloMalto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMaltoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloMaltoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/maltoza/HelloMalto/HelloMaltoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maltoza.HelloMalto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMaltoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
